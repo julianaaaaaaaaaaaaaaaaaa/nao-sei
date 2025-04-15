d33c8b8fa74a7eb5f4525eb05a93a5e48ec8df1c
julianaaaaaaaaaaaaaaaaaa <juliana.paiva@escolar.ifrn.edu.br>
2025-04-15 14:24:43 +0000

    oi

---
+++ aula02/lista02_ex03.py
@@ -0,0 +1,7 @@
+nota1= int(input("Digite a nota do primeiro bimestre da disciplina: "))
+
+nota2 = int(input("Digite a nota do segundo bimestre da disciplina: "))
+
+media_parcial = (nota1 * 2 + nota2 * 3) / 5
+
+print(f"Média parcial = {int(media_parcial)}")

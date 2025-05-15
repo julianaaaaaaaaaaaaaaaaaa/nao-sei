4a5a37a4f9a213732c961b7368069c4e502c491b
julianaaaaaaaaaaaaaaaaaa <juliana.paiva@escolar.ifrn.edu.br>
2025-05-15 13:12:49 +0000

    oi

--- aula 08/ex_01.py
+++ aula 08/ex_01.py
@@ -1,12 +1,27 @@
 class Agua:
-    def __init__(self):
-        self.mes = 1
-        self.ano = 2025
-        self.consumo = 0
+    def __init__(self):    # Encapsulamento
+        self.__mes = 1
+        self.__ano = 2025
+        self.__consumo = 0
+    def set_mes(self, v):  # método de acesso: set para ajustar o valor do atributo
+        if v < 1 or v > 12: raise ValueError("O mês deve estar entre 1 e 12")   # validação
+        self.__mes = v  
+    def set_ano(self, v):
+        if v == 0: raise ValueError("O ano não pode ser zero")                  # validação
+        self.__ano = v
+    def set_consumo(self, v):
+        if v < 0: raise ValueError("O consumo não pode ser negativo")           # validação
+        self.__consumo = v   
+    def get_mes(self):     # método de acesso: get para retornar o valor do atributo
+        return self.__mes           
+    def get_ano(self):
+        return self.__ano           
+    def get_consumo(self):
+        return self.__consumo           
     def valor(self):
-        if self.consumo <= 10: return 38
-        if 11 <= self.consumo <= 20: return 38 + (self.consumo - 10) * 5
-        if self.consumo > 20: return 38 + 50 + (self.consumo - 20) * 6  
+        if self.__consumo <= 10: return 38
+        if 11 <= self.__consumo <= 20: return 38 + (self.__consumo - 10) * 5
+        if self.__consumo > 20: return 38 + 50 + (self.__consumo - 20) * 6  
 
 class Triangulo():
     def __init__(self):
@@ -31,10 +46,21 @@ class UI: # UI = User Interface: print e input
     @staticmethod
     def agua():
         x = Agua() # x é um objeto da classe Água
-        x.mes = int(input("Informe o mês da conta: "))
-        x.ano = int(input("informe o ano: "))
-        x.consumo = int(input("informe o consumo em m3: "))
-        print(f"O valor da conta de água do mês {x.mes} do ano {x.ano} é {x.valor()}")
+
+        # x.mes = int(input("Informe o mês da conta: "))
+        x.set_mes(int(input("Informe o mês da conta: ")))
+
+        # x.ano = int(input("informe o ano: "))
+        x.set_ano(int(input("informe o ano: ")))
+
+        # x.consumo = int(input("informe o consumo em m3: "))
+        x.set_consumo(int(input("informe o consumo em m3: ")))
+        
+        #print(x.__mes, x.__ano, x.__consumo)
+        # __mes, __ano e __consumo não são visíveis
+
+        #print(f"O valor da conta de água do mês {x.mes} do ano {x.ano} é {x.valor()}")
+        print(f"O valor da conta de água do mês {x.get_mes()} do ano {x.get_ano()} é {x.valor()}")
     @staticmethod
     def triangulo():
         x = Triangulo() # x é um objeto da classe Triangulo

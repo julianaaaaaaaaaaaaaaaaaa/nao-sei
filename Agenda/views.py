fdec1a44042a20686b03cf413b9419331df5f6b5
julianaaaaaaaaaaaaaaaaaa <juliana.paiva@escolar.ifrn.edu.br>
2025-09-24 19:08:43 +0000

    oi

--- Agenda/views.py
+++ Agenda/views.py
@@ -19,3 +19,21 @@ class View:
     def cliente_excluir(id):
         cliente = Cliente(id, "", "", "")
         ClienteDAO.excluir(cliente)
+
+    def servico_listar():
+        return ServicoDAO.listar()
+
+    def servico_listar_id(id):
+        return ServicoDAO.listar_id(id)
+
+    def servico_inserir(descricao, valor):
+        servico = Servico(0, descricao, valor)
+        ServicoDAO.inserir(servico)
+
+    def servico_atualizar(id, descricao, valor):
+        servico = Servico(id, descricao, valor)
+        ServicoDAO.atualizar(servico)
+
+    def servico_excluir(id):
+        servico = Servico(id, "", 0.0)
+        ServicoDAO.excluir(servico)

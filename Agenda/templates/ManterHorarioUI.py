7d6125ffb1bff72b6b622b70a18327ece4d983fb
julianaaaaaaaaaaaaaaaaaa <juliana.paiva@escolar.ifrn.edu.br>
2025-10-16 12:30:05 +0000

    oi

--- Agenda/templates/ManterHorarioUI.py
+++ Agenda/templates/ManterHorarioUI.py
@@ -22,15 +22,20 @@ class ManterHorarioUI:
             for obj in horarios:
                 cliente = View.cliente_listar_id(obj.get_id_cliente())
                 servico = View.servico_listar_id(obj.get_id_servico())
+                profissional = View.profissional_listar_id(obj.get_id_profissional())  
+
                 if cliente != None: cliente = cliente.get_nome()
                 if servico != None: servico = servico.get_descricao()
+                if profissional != None: profissional = profissional.get_nome()   
+
                 dic.append({
-                    "id": obj.get_id(),
-                    "data": obj.get_data(),
-                    "confirmado": obj.get_confirmado(),
-                    "cliente": cliente,
-                    "serviço": servico
-                })
+                "id": obj.get_id(),
+                "data": obj.get_data(),
+                "confirmado": obj.get_confirmado(),
+                "cliente": cliente,
+                "serviço": servico,
+                "profissional": profissional  
+            })
             df = pd.DataFrame(dic)
             st.dataframe(df)
 
@@ -48,7 +53,7 @@ class ManterHorarioUI:
         if st.button("Inserir"):
             id_cliente = cliente.get_id() if cliente else None
             id_servico = servico.get_id() if servico else None
-            profissional = profissional.get_id() if profissional else None  
+            id_profissional = profissional.get_id() if profissional else None  
             View.horario_inserir(datetime.strptime(data, "%d/%m/%Y %H:%M"), confirmado, id_cliente, id_servico)
             st.success("Horário inserido com sucesso")
             time.sleep(2)
@@ -61,20 +66,23 @@ class ManterHorarioUI:
         else:
             clientes = View.cliente_listar()
             servicos = View.servico_listar()
-            profissional = View.profissional_listar() 
+            profissionais = View.profissional_listar() 
             op = st.selectbox("Atualização de Horários", horarios)
             data = st.text_input("Informe a nova data e horário do serviço", op.get_data().strftime("%d/%m/%Y %H:%M"))
             confirmado = st.checkbox("Nova confirmação", op.get_confirmado())
             id_cliente = None if op.get_id_cliente() in [0, None] else op.get_id_cliente()
             id_servico = None if op.get_id_servico() in [0, None] else op.get_id_servico()
-            
+            id_profissional = op.get_id_profissional()  
 
             cliente = st.selectbox("Informe o novo cliente", clientes, next((i for i, c in enumerate(clientes) if c.get_id() == id_cliente), None))
             servico = st.selectbox("Informe o novo serviço", servicos, next((i for i, s in enumerate(servicos) if s.get_id() == id_servico), None))
+            profissional = st.selectbox("Novo profissional", profissionais, next((i for i, p in enumerate(profissionais) if p.get_id() == id_profissional), None))  
+
 
             if st.button("Atualizar"):
                 id_cliente = cliente.get_id() if cliente else None
                 id_servico = servico.get_id() if servico else None
+                id_profissional = profissional.get_id() if profissional else None
                 View.horario_atualizar(op.get_id(), datetime.strptime(data, "%d/%m/%Y %H:%M"), confirmado, id_cliente, id_servico)
                 st.success("Horário atualizado com sucesso")
                 time.sleep(2)
